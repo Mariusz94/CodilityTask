d50c3f2cddd50a392dc272e2c7a04b5dcd71d7cf
Mariusz Lyszczarz <lyszczarz.mariusz@gmail.com>
2018-05-18 12:46:58 +0200

    nextcodiity

--- src/FrogRiverOne/FrogRiverOne.java
+++ src/FrogRiverOne/FrogRiverOne.java
@@ -1,21 +1,37 @@
 package FrogRiverOne;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class FrogRiverOne {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] a = {1,3,1,4,2,3,5,4};
-        System.out.println(solution.solution(8, a));
+        int[] a = {1, 3, 1, 4, 3, 5, 4, 2};
+        System.out.println(solution.solution(4, a));
     }
 }
 
 class Solution {
     public int solution(int X, int[] A) {
         // write your code in Java SE 8
+        Set<Integer> a = new HashSet<>();
+        for (int i = 0; i < X; i++) {
+            a.add(A[i]);
+        }
 
+        int counter = 1;
+        int nextNumber = 0;
+        while (counter <= X) {
+
+            if (a.contains(counter)) {
+                counter++;
+                continue;
+            }
+
+            if (A.length == (X+nextNumber)) return -1;
+
+            a.add(A[X + nextNumber]);
+            nextNumber++;
+        }
+        return X + --nextNumber;
     }
 }
