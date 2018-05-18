49dde2662b62ac2e4ba9b2a0f50f629d2b569241
Mariusz Lyszczarz <lyszczarz.mariusz@gmail.com>
2018-05-18 12:54:07 +0200

    nextCodiity

--- src/FrogRiverOne/FrogRiverOne.java
+++ src/FrogRiverOne/FrogRiverOne.java
@@ -5,14 +5,15 @@ import java.util.*;
 public class FrogRiverOne {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] a = {1, 3, 1, 4, 3, 5, 4, 2};
-        System.out.println(solution.solution(4, a));
+        int[] a = {3};
+        System.out.println(solution.solution(5, a));
     }
 }
 
 class Solution {
     public int solution(int X, int[] A) {
         // write your code in Java SE 8
+        if(A.length<X) return -1;
         Set<Integer> a = new HashSet<>();
         for (int i = 0; i < X; i++) {
             a.add(A[i]);
